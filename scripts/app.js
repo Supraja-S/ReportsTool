7999d624979f326bb9fe3f2a6110db03d499df8f
supraja <ssupraja@deloitte.com>
2017-02-15 08:21:31 +0530

    Impact analysis layout

--- scripts/app.js
+++ scripts/app.js
@@ -6,7 +6,7 @@ reportsTool.config(function($routeProvider,$locationProvider) {
         templateUrl : "../partials/dashboard.html",
 	})
 	.when("/impact", {
-        templateUrl : "../partials/businessProcess.html",
+        templateUrl : "../partials/impactAnalysis.html",
 	})
 	.when("/sFour", {
         templateUrl : "../partials/s4Hana.html",
@@ -72,4 +72,71 @@ reportsTool.controller('bpController',['$scope',function($scope){
 			
 		}
 	]
+}]);
+
+reportsTool.controller('ImpactController',['$scope',function($scope){
+	$scope.selected = 'defects';
+	
+	$scope.IncompabilitycountsArray = [
+	{
+		count:100,
+		color:'white',
+		description:'Total Defects'
+	},
+	{
+		color:'red',
+		count:33,
+		description:'High impact'
+	},
+	{
+		count:33,
+		color:'yellow',
+		description:'Medium impact'
+	},
+	{
+		count:3,
+		color:'green',
+		description:'Low impact'
+	}
+	
+	]
+	$scope.tabularData = [
+	{
+			name:'object1',
+			value1:'value1',
+			value2:'value2'
+			
+		},
+		{
+			name:'object2',
+			value1:'value1',
+			value2:'value2'
+			
+		},
+		{
+			name:'object3',
+			value1:'value1',
+			value2:'value2'
+			
+		},
+		{
+			name:'object4',
+			value1:'value1',
+			value2:'value2'
+			
+		},
+		{
+			name:'object5',
+			value1:'value1',
+			value2:'value2'
+			
+		},
+		{
+			name:'object1',
+			value1:'value1',
+			value2:'value2'
+			
+		}
+	];
+	
 }]);
