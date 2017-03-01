f6288cc15331053ba3d187f05ad332db1ad5507d
supraja <ssupraja@deloitte.com>
2017-03-01 20:03:13 +0530

    Added changes to server

--- scripts/util.js
+++ scripts/util.js
@@ -1,5 +1,34 @@
+var  mapTextFile;
+var S4Data;
+var request = $.ajax({
+  url: "/getFileContent",
+  method: "GET",
+  data: { fileName : 'data/SAPUATProp.Properties' },
+});
+request.done(function( response ) {
+  mapTextFile = response;
+});
+request.fail(function( jqXHR, textStatus ) {
+  console.log('error');
+});
+getS4Data();
 
-var  mapTextFile = getFileContent("data/SAPUATProp.Properties");
+function getS4Data(){
+	
+	var request = $.ajax({
+	  url: "/getFileContent",
+	  method: "GET",
+	  data: { fileName : 'data/BF_Check_20161107114158.txt' },
+	});
+	request.done(function( response ) {
+	  S4Data = response;
+	  console.log(S4Data);
+	});
+	request.fail(function( jqXHR, textStatus ) {
+	  console.log('error');
+	});
+
+}
 
 function getFileName(key){
 	var fileName =null;
