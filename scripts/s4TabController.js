dd21bc36b443398dc49f4494e9c44785ef3b1a8b
Farah <farashaikh@deloitte.com>
2017-07-14 18:59:13 +0530

    S4 code clean up - removed commented code

--- scripts/s4TabController.js
+++ scripts/s4TabController.js
@@ -3,66 +3,14 @@ reportsTool.controller('s4Controller',['$scope','getFileContent',function($scope
   	$scope.reverseSort = false;
 	$scope.selected = 'busFunctions';
 
-    //$scope.supportedCount = 0;
-    //$scope.notSupportedCount = 0;
-
     $scope.tabularData = '';
 	$scope.chart={
-		view:'',
-		callBackCheck:false,
-		piechart:{
-			options:[],
-			data:[]
-		},
-		linechart:{
-			options:[],
-			data:[]
-		},
-		barchart:{
-			options:[],
-		}
+		view1:'pieChart',
+        view2:'pieChart'
 	};
 
     $scope.searchCategory = "COMPONENT";
     $scope.searchText = "";
-    /*$scope.floatTheadOptions = {
-        scrollContainer: function($table){
-            return $table.closest('.center-section');
-        }
-    };*/
-
-    /*$scope.filterFunction = function(item) {
-        var val = item[$scope.searchCategory].toLowerCase();
-        return (val.indexOf($scope.searchText.toLowerCase()) > -1);
-    };*/
-
-	/*s4TabService.getData(getFileName('S4HANA_DATA')).then(function(response){
-		$scope.tabularData = response;
-
-        $scope.uniqueBFuncCateg = [];
-        angular.forEach($scope.tabularData, function(value, index){
-            if(value.CURRSTAT == 'ON'){
-                var found = false;
-                for(var i=0; i<$scope.uniqueBFuncCateg.length; i++){
-                    if($scope.uniqueBFuncCateg[i].key == value.BFUNCCATEG){
-                        $scope.uniqueBFuncCateg[i].y++;
-                        found = true;
-                        break;
-                    }
-                }
-
-                if(!found){
-                    $scope.uniqueBFuncCateg.push({key:value.BFUNCCATEG, y:1})
-                }
-            }
-            if(value.FUTURESTAT == 'ON'){
-                $scope.supportedCount++;
-            }
-            else if(value.FUTURESTAT == 'OFF'){
-                $scope.notSupportedCount++;
-            }
-        });
-	});*/
 
     getFileContent.getData(getFileName('S4HANA_COUNT_SUMMARY')).then(function(response){
         $scope.countSummary = response;
@@ -75,12 +23,6 @@ reportsTool.controller('s4Controller',['$scope','getFileContent',function($scope
     getFileContent.getData(getFileName('S4HANA_BF_BY_COMPTYPE')).then(function(response){
         $scope.uniqueBFuncCompType = response;
     });
-
-	
-    /*$scope.sortFunction = function(key){
-        $scope.orderByField = key;
-        $scope.reverseSort = !$scope.reverseSort;
-    };*/
 
 	 $scope.options = {
         chart: {
